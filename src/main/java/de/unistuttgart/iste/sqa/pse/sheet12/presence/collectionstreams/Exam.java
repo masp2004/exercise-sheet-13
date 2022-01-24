1a596b83ece88adc7e3c20625120d770ab0ce717
Marcel Graf <91905278+MarcelGraf0710@users.noreply.github.com>
2022-01-24 20:53:10 +0100

    Added JML comments

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet12/presence/collectionstreams/Exam.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet12/presence/collectionstreams/Exam.java
@@ -2,12 +2,13 @@ package de.unistuttgart.iste.sqa.pse.sheet12.presence.collectionstreams;
 
 import java.time.LocalDate;
 
-
+/*@
+  @ invariant subject != null;
+  @ invariant mark > 1.0 && mark < 6.0;
+  @ invariant date != null;
+  @*/
 /**
- * 
  * Represents an exam. 
- * 
- * Invariant: subject != null, mark set, date != null
  * 
  * @author mfrank
  *
@@ -18,12 +19,18 @@ public class Exam {
 	private final double mark;
 	private final String subject;
 	
+	/*@
+	  @ requires mark > 1.0 && mark < 6.0;
+	  @ requires subject != null;
+	  @*/
 	/**
 	 * Constructor  
-	 * @param mark - must between 1.0 and 6.0
-	 * @param subject - must be not null
+	 * @param mark the mark achieved in this exam (between 1.0 and 6.0)
+	 * @param subject subject of this exam (must not be null)
+	 * @throws IllegalArgumentException if the specified mark is lower than 1.0 
+	 *	   or greater than 6.0, or if the subject is {@code null}.
 	 */
-	public Exam(final double mark, final String subject) {
+	public Exam(final double mark, final String subject) throws IllegalArgumentException {
 		if(mark < 1.0 || mark > 6.0 || subject == null) {
 			throw new IllegalArgumentException();
 		}
@@ -32,25 +39,34 @@ public class Exam {
 		this.mark = mark;
 		this.subject = subject;
 	}
-
+	
+	/*@
+	  @ ensures \result == date;
+	  @*/
 	/**
 	 * @return the date when the exam was created
 	 */
-	public LocalDate getDate() {
+	public /*@ pure @*/ LocalDate getDate() {
 		return date;
 	}
 
+	/*@
+	  @ ensures \result == mark;
+	  @*/
 	/**
 	 * @return returns the mark of the exam
 	 */
-	public double getMark() {
+	public /*@ pure @*/ double getMark() {
 		return mark;
 	}
 
+	/*@
+	  @ ensures \result == subject;
+	  @*/
 	/**
 	 * @return returns the subject
 	 */
-	public String getSubject() {
+	public /*@ pure @*/ String getSubject() {
 		return subject;
 	}
 }
