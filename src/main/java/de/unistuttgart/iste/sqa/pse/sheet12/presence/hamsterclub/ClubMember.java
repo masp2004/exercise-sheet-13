d742af27c527bfc2b5004803c19c7b365f00d68d
Marcel Graf <91905278+MarcelGraf0710@users.noreply.github.com>
2022-01-26 21:20:50 +0100

    Update ClubMember.java

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet12/presence/hamsterclub/ClubMember.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet12/presence/hamsterclub/ClubMember.java
@@ -1,9 +1,8 @@
 package de.unistuttgart.iste.sqa.pse.sheet12.presence.hamsterclub;
 
 /**
- * Individual member of a Club. 
+ * This class models an individual member of a club. 
  * @see de.unistuttgart.iste.sqa.pse.sheet12.presence.hamsterclub.HamsterClub
- * 
  */
 public class ClubMember implements Comparable<ClubMember> {
 	
@@ -13,8 +12,7 @@ public class ClubMember implements Comparable<ClubMember> {
 	private boolean hasPaidFees;
 	
 	/*@
-	  @ requires name != null;
-	  @ requires contributions >= 0;
+	  @ requires name != null && age >= 0 && contributions >= 0;
  	  @*/
 	/**
 	 * Constructor of a club member.
@@ -22,11 +20,11 @@ public class ClubMember implements Comparable<ClubMember> {
 	 * @param age
 	 * @param contributions
 	 * @param hasPaidFees
-	 * @throws IllegalArgumentException if name is {@code null} or the contributions are negative.
+	 * @throws IllegalArgumentException if name is {@code null} or the any of age and contributions is negative.
 	 */
 	public ClubMember(final String name, final int age, final double contributions, final boolean hasPaidFees) 
 		throws IllegalArgumentException {
-		if(name == null || contributions < 0){
+		if(name == null || age < 0 ||contributions < 0){
 			throw new IllegalArgumentException();
 		}
 		this.name = name;
@@ -36,20 +34,20 @@ public class ClubMember implements Comparable<ClubMember> {
 	}
 	
 	/**
-	 * Overridden equals method to enable proper comparisons
+	 * Checks if the speficied object equals the club member on which it is called.
 	 */
 	@Override
-	public boolean equals(final Object obj) { 
+	public boolean equals(final Object object) { 
 		  
-        if (obj == this) { 
+        if (object == this) { 
             return true; 
         } 
           
-        if (!(obj instanceof ClubMember)) { 
+        if (!(object instanceof ClubMember)) { 
             return false; 
         } 
           
-        ClubMember member = (ClubMember) obj; 
+        ClubMember member = (ClubMember) object; 
           
         return member.getName().equals(name) &&
         	   member.getAge() == age &&
@@ -58,35 +56,59 @@ public class ClubMember implements Comparable<ClubMember> {
     } 
  
 	/**
-	 * Overridden compareTo method for use with clubMemberComparator
+	 * Overridden compareTo method for use with clubMemberComparator. It compares members based on their age.
 	 */
 	@Override
-	public int compareTo(final ClubMember arg0) {
-		return this.age - arg0.getAge();
+	public int compareTo(final ClubMember otherMember) {
+		return this.age - otherMember.getAge();
 	}
 		
 	public /*@ pure @*/ String getName() {
 		return name;
 	}
-	public void setName(String name) {
+	
+	/*@
+	  @ requires name != null;
+	  @*/
+	public void setName(final String name) {
+		if(name != null){
+			throw new IllegalArgumentException();	
+		}
 		this.name = name;
 	}
 	public /*@ pure @*/ int getAge() {
 		return age;
 	}
-	public void setAge(int age) {
+	
+	/*@
+	  @ requires age > 0;
+	  @*/
+	public void setAge(final int age) {
+		if(age <= 0){
+			throw new IllegalArgumentException();	
+		}
 		this.age = age;
 	}
+	
 	public /*@ pure @*/ double getContributions() {
 		return contributions;
 	}
-	public void setContributions(Double contributions) {
+	
+	/*@
+	  @ requires contributions >= 0;
+	  @*/
+	public void setContributions(final double contributions) {
+		if(contributions < 0){
+			throw new IllegalArgumentException();
+		}
 		this.contributions = contributions;
 	}
+	
 	public /*@ pure @*/ boolean getHasPaidContributions() {
 		return hasPaidFees;
 	}
-	public void setHasPaidContributions(boolean hasPaidFees) {
+	
+	public void setHasPaidContributions(final boolean hasPaidFees) {
 		this.hasPaidFees = hasPaidFees;
 	}
 
