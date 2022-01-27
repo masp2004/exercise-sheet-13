5e45b26e61609b2dc99063ea29ebc9557914ed94
st167057 <fabi.schmierer@gmail.com>
2022-01-27 20:08:11 +0100

    Little refactoring bugfix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,5 @@
 open module exercise {
-    exports de.unistuttgart.iste.sqa.pse.sheet12.presence.hamsterclubtest;
+    exports de.unistuttgart.iste.sqa.pse.sheet12.presence.hamsterclub;
 	requires de.hamstersimulator.objectsfirst.main;
 	requires de.hamstersimulator.objectsfirst.core;
 }
