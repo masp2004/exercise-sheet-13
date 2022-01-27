2dd9364b825114168b91acf7923b2f3de2fd75ed
st167057 <fabi.schmierer@gmail.com>
2022-01-27 21:29:22 +0100

    Comment to RegistrationApp added

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet13/presence/registration/RegistrationApp.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet13/presence/registration/RegistrationApp.java
@@ -1,5 +1,9 @@
 package de.unistuttgart.iste.sqa.pse.sheet13.presence.registration;
 
+/**
+ * This class contains the {@code main}-method, responsible for starting the GUI.
+ * Do not modify!
+ */
 public class RegistrationApp {
 	public static void main (String[] args) {
 		RegistrationGUI myGUI = new RegistrationGUI();
