f0496fabf3a017f5bb785d369d421de800a3f3aa
franksn90 <s.frankausowen@googlemail.com>
2022-02-01 17:23:27 +0100

    Removed unnecessary local variable

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet13/presence/registration/RegistrationApp.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet13/presence/registration/RegistrationApp.java
@@ -6,6 +6,6 @@ package de.unistuttgart.iste.sqa.pse.sheet13.presence.registration;
  */
 public class RegistrationApp {
 	public static void main (String[] args) {
-		RegistrationGUI myGUI = new RegistrationGUI();
+		new RegistrationGUI();
 	}
 }
