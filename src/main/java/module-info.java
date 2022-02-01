5f0e61816a84cef925a0ea53aa3166429f55dfc8
franksn90 <s.frankausowen@googlemail.com>
2022-02-01 17:25:17 +0100

    Remove unnecessary module file

--- src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-open module exercise {
-    exports de.unistuttgart.iste.sqa.pse.sheet13.presence.registration;
-	requires de.hamstersimulator.objectsfirst.main;
-	requires de.hamstersimulator.objectsfirst.core;
-	requires java.desktop;
-}
